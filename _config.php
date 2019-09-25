8331a00c04a0828215549af0782323536a2b3bdb
Ralph Slooten <axllent@gmail.com>
2019-09-25 16:50:45 +1200

    Support php 7.3

--- _config.php
+++ _config.php
@@ -1,5 +1,5 @@
 <?php
 
-Object::add_extension('ModelAdmin', 'GridFieldPaginatorHeaderExtension');
+SS_Object::add_extension('ModelAdmin', 'GridFieldPaginatorHeaderExtension');
 
 LeftAndMain::require_css(basename(dirname(__FILE__)) . '/css/paginatorheader.css');
